139450b1cba772699d8324911249c47ba51b479f
Mustafa Hussin <info@og-coding.net>
2024-11-05 18:03:50 +0200

    updata_footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ const Footer = () => {
     <div className="w-full h-20 bg-amazon_light text-gray-300 flex items-center justify-center gap-4">
       <Image className="w-24" src={logo} alt="logo" />
       <p className="text-sm -mt-4">
-        All rights reserved{" "}
+        All rights reserved{"2024 "}
         <a
           className="hover:text-white hover:underline decoration-[1px] cursor-pointer duration-300"
           href="https://reactbd.com"
           target="_blank"
         >
-          @reactbd.com
+          og-coding.net
         </a>
       </p>
     </div>
